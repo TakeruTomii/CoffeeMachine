af10386b3c01f865b87725dba6ea1c046bf7bc8b
TakeruTomii <dev.haunted.house@gmail.com>
2024-05-13 14:47:01 +1200

    implement controller

--- Brewer/BrewController.cs
+++ Brewer/BrewController.cs
@@ -9,25 +9,32 @@ namespace CoffeeMachine.Brewer
     public class BrewController : ControllerBase
     {
         private readonly ILogger<BrewController> _logger;
+        private readonly IBrewService _brewService;
 
-        public BrewController(ILogger<BrewController> logger)
+        public BrewController(
+            ILogger<BrewController> logger,
+            IBrewService brewService)
         {
             _logger = logger;
+            _brewService = brewService;
         }
 
         [HttpGet]
         [Route("brew-coffee")]
-        public Coffee BrewCoffee()
+        public ActionResult<Coffee> BrewCoffee()
         {
             try
             {
-                return ;
-            } 
-            catch (Exception ex)
-            {
-
+                return _brewService.Brew();
             }
-
+            catch (TeaPotException ex)
+            {
+                return StatusCode(418, value: ex.Message);
+            }
+            catch (OutOfCoffeeException ex)
+            {
+                return StatusCode(503, value: ex.Message);
+            }
         }
     }
 }
