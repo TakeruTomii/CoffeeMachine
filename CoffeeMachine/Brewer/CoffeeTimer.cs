92e189b811801317b8d4810692bbbef80541b8dc
TakeruTomii <dev.haunted.house@gmail.com>
2024-05-13 17:28:13 +1200

    Add coffee timer test

--- CoffeeMachine/Brewer/CoffeeTimer.cs
+++ CoffeeMachine/Brewer/CoffeeTimer.cs
@@ -4,7 +4,7 @@ namespace CoffeeMachine.Brewer
 {
     public class CoffeeTimer : ICoffeeTimer
     {
-        private const string ISO_8601_DATE_FORMAT = "yyyy-MM-ddTHH:mm:ssZ";
+        private const string ISO_8601_DATE_FORMAT = "yyyy-MM-ddTHH:mm:sszzz";
         public string GetPreparedTime()
         {
             return DateTime.UtcNow.ToString(ISO_8601_DATE_FORMAT);
---
+++ CoffeeMachineTest/CoffeeTimerTests.cs
@@ -0,0 +1,26 @@
+﻿using CoffeeMachine.Brewer;
+using System.Globalization;
+
+namespace CoffeeMachineTest
+{
+    [TestClass]
+    public class CoffeeTimerTests
+    {
+        private const string ISO_PATTERN = "yyyy-MM-ddTHH:mm:sszzz";
+
+        [TestMethod]
+        public void GetPreparedTime_ExcecuteOnce_ReturnISOFomatString()
+        {
+            var _timer = new CoffeeTimer();
+            var res = _timer.GetPreparedTime();
+
+            Assert.IsTrue(IsIso8601DateTime(res));
+        }
+
+        static bool IsIso8601DateTime(string input)
+        {
+            DateTime result;
+            return DateTime.TryParseExact(input, ISO_PATTERN, CultureInfo.InvariantCulture, DateTimeStyles.None, out result);
+        }
+    }
+}
