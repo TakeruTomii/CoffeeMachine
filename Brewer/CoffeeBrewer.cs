0f8f63d36e1720757242d5bb71e562c573a4fbf7
TakeruTomii <dev.haunted.house@gmail.com>
2024-05-13 13:52:54 +1200

    Implement Coffee Brewer

---
+++ Brewer/CoffeeBrewer.cs
@@ -0,0 +1,18 @@
+﻿namespace CoffeeMachine.Brewer
+{
+    public class CoffeeBrewer : ICoffeeBrewer
+    {
+        private static int counter = 0;
+        private const int MAX_BREW = 5;
+        public bool IsSuccessfullyBrewed()
+        {
+            counter++;
+            if(counter >= MAX_BREW)
+            {
+                counter = 0;
+                return false;
+            }
+            return true;
+        }
+    }
+}
