a5ffc176de07c8bcca20d8dd3b24ba5d3efb9a67
TakeruTomii <dev.haunted.house@gmail.com>
2024-05-13 13:37:35 +1200

    add coffee timer

---
+++ Brewer/CoffeeTimer.cs
@@ -0,0 +1,10 @@
+﻿namespace CoffeeMachine.Brewer
+{
+    public class CoffeeTimer : ICoffeeTimer
+    {
+        public string GetPreparedTime()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
