55ea89e46b519c1ebd6b4a5ba62c475256dcc019
TakeruTomii <dev.haunted.house@gmail.com>
2024-05-14 11:36:48 +1200

    Add http client service

---
+++ CoffeeMachine/Infrastracuture/HttpClientService.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using System.Xml.Linq;
+
+namespace CoffeeMachine.Infrastracuture
+{
+    public class HttpClientService
+    {
+        readonly HttpClient _httpClient;
+        async Task<T> GetAsync<T>(Uri url)
+        {
+            var res = await _httpClient.GetAsync(url);
+
+            res.EnsureSuccessStatusCode();
+
+            var json = await res.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
