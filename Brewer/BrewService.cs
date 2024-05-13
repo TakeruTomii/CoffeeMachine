ed90fdc439468fc9dab27251c44b83520e156385
TakeruTomii <dev.haunted.house@gmail.com>
2024-05-13 14:34:20 +1200

    implement brew service

--- Brewer/BrewService.cs
+++ Brewer/BrewService.cs
@@ -1,10 +1,44 @@
-﻿namespace CoffeeMachine.Brewer
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace CoffeeMachine.Brewer
 {
     public class BrewService : IBrewService
     {
+        private const string BREW_MESSAGE = "Your piping hot coffee is ready";
+
+        private readonly ICoffeeTimer _timer;
+        private readonly ICoffeeBrewer _brewer;
+        public BrewService(ICoffeeTimer timer, ICoffeeBrewer brewer)
+        {
+            _timer = timer;
+            _brewer = brewer;
+        }
         public Coffee Brew()
         {
-            throw new NotImplementedException();
+            var preparedTime = _timer.GetPreparedTime();
+            if (IsAprilFool(preparedTime))
+            {
+                throw new TeaPotException();
+            }
+
+            if (!_brewer.IsSuccessfullyBrewed())
+            {
+                throw new OutOfCoffeeException();
+            }
+
+            var res = new Coffee
+            {
+                message = BREW_MESSAGE,
+                prepared = preparedTime
+            };
+
+            return res;
+        }
+
+        private bool IsAprilFool(string dateTime)
+        {
+            var date = DateTime.Parse(dateTime);
+            return date.Day == 1 && date.Month == 4;
         }
     }
 }
