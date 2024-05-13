060bf9e55ba520378d594e69380cd5f85bf087a6
TakeruTomii <dev.haunted.house@gmail.com>
2024-05-14 11:37:03 +1200

    add class for http response

---
+++ CoffeeMachine/Brewer/Model/Weather.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CoffeeMachine.Brewer.Model
+{
+    public class Weather
+    {
+        [JsonPropertyName("main.temp")]
+        public int Temperature { get; set; }
+    }
+}
