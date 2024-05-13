883f240b9ea3ace528216c3b69e0ec19bde00f87
TakeruTomii <dev.haunted.house@gmail.com>
2024-05-13 13:36:04 +1200

    add controller

---
+++ Brewer/BrewController.cs
@@ -0,0 +1,33 @@
+﻿using CoffeeMachine.Controllers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeMachine.Brewer
+{
+    [Route("brew-coffee")]
+    [ApiController]
+    public class BrewController : ControllerBase
+    {
+        private readonly ILogger<BrewController> _logger;
+
+        public BrewController(ILogger<BrewController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("brew-coffee")]
+        public Coffee BrewCoffee()
+        {
+            try
+            {
+                return new Coffee();
+            } 
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+    }
+}
